5c03cdcbb292358654e389fd17d955b728c97469
Yoriaantje <1904507peters@zuyd.nl>
2022-02-18 13:16:06 +0100

    Update install_deps.py
    Now updates PIP and can be run with all programs

--- install_deps.py
+++ install_deps.py
@@ -2,10 +2,34 @@ import subprocess
 import sys
 
 
+package_list = [
+    "mediapipe",
+    "opencv-python"
+    ]
+
+
+def update_pip():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
+def auto_install(_package_list):
+    update_pip()
+    for p in _package_list:
+        try:
+            install(p)
+        except:
+            print("Something went wrong installing: ", p)
+
+
 if __name__ == '__main__':
-    install("mediapipe")
-    install("cv2")
+    auto_install(package_list)
+    print("Installed dependencies!")
+
+
+else:
+    auto_install(package_list)
+    print("Installed dependencies!")
