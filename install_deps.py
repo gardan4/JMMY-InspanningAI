0f8f7af2072d8061eca5fadee0a04ac245938774
Yoriaantje <1904507peters@zuyd.nl>
2022-02-18 13:03:00 +0100

    Run this to install dependencies
    Using this script, dependencies will be installed automagically using PIP.
    Test for PyCharm.

---
+++ install_deps.py
@@ -0,0 +1,11 @@
+import subprocess
+import sys
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+if __name__ == '__main__':
+    install("mediapipe")
+    install("cv2")
