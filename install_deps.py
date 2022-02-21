498d6244ecdda8e28c4f3fa46c8b155c3cad1c1b
Yoriaantje <1904507peters@zuyd.nl>
2022-02-21 16:27:27 +0100

    Minor changes because menu

--- install_deps.py
+++ install_deps.py
@@ -16,20 +16,24 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
+def unattended_install():
+    update_pip()
+    for p in package_list:
+        try:
+            install(p)
+        except:
+            print("Something went wrong installing: \n", p)
+
+
 def auto_install(_package_list):
     update_pip()
     for p in _package_list:
         try:
             install(p)
         except:
-            print("Something went wrong installing: ", p)
+            print("Something went wrong installing: \n", p)
 
 
 if __name__ == '__main__':
-    auto_install(package_list)
-    print("Installed dependencies!")
-
-
-else:
     auto_install(package_list)
     print("Installed dependencies!")
