7eb7664c8740cb0618e36e81bd2a250999089f55
Yoriaantje <1904507peters@zuyd.nl>
2022-03-07 12:07:24 +0100

    Update generate_data_from_video.py

--- generate_data_from_video.py
+++ generate_data_from_video.py
@@ -2,6 +2,13 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import sys
+import os
+
+
+def auto_run(_video_path):
+    mp_d, mp_p, p, c = startup(video_path)
+    o = output(video_path, c)
+    program(p, c, o, mp_p, mp_d)
 
 
 def startup(_video_path):
@@ -17,7 +24,7 @@ def startup(_video_path):
         print("Error opening video stream or file")
         raise TypeError
 
-    return mp_drawing, pose, cap
+    return mp_drawing, mp_pose, pose, cap
 
 
 def output(_video_path, cap):
@@ -29,16 +36,25 @@ def output(_video_path, cap):
 
     inflnm, inflext = inputflnm.split('.')
 
-    out_filename = f'{outdir}{inflnm}_annotated.{inflext}'
+    try:
+        # creating a folder named data
+        if not os.path.exists('generateData_OUTPUT'):
+            os.makedirs('generateData_OUTPUT')
+
+    # if not created then raise error
+    except OSError:
+        print("Error: Can't create a folder here.")
+
+    out_filename = f'{outdir}generateData_OUTPUT/{inflnm}_annotated.{inflext}'
     out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc(
         'M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
     return out
 
 
-def program(cap):
+def program(pose, cap, out, mp_pose, mp_drawing):
     while cap.isOpened():
-        succes, image = cap.read()
-        if not succes:
+        success, image = cap.read()
+        if not success:
             break
 
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
@@ -57,9 +73,10 @@ def program(cap):
 
 
 if __name__ == '__main__':
+    print("File: ")
     video_path = input()
-    mediapipe_drawing, mediapipe_pose, capture = startup(video_path)
+    mediapipe_drawing, mediapipe_pose, posevar, capture = startup(video_path)
     output = output(video_path, capture)
-    program(capture)
+    program(posevar, capture, output, mediapipe_pose, mediapipe_drawing)
 
 
