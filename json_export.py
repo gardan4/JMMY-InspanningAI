877dd12665f7066e78d1b365f65917335b3deeb2
MaartenMaessen <1916580maessen@zuyd.nl>
2022-03-08 15:05:54 +0100

    upload

---
+++ json_export.py
@@ -0,0 +1,14 @@
+import json
+
+def export(to_export):
+    try:
+        j = json.dumps(to_export)
+        with open('export.json', 'w') as f:
+            f.write(j)
+            f.close()
+
+    except:
+        print("error")
+
+
+2
