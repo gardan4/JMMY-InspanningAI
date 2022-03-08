3f4389faa388df9173a84fa387f5e40b2524f578
gardan4 <m-m2001@live.nl>
2022-03-08 15:20:59 +0100

    working vid facemeshing

--- generate_data_from_video.py
+++ generate_data_from_video.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import numpy as np
 import sys
 import os
+import time
 
 
 def auto_run(_video_path):
@@ -28,9 +29,10 @@ def startup(_video_path):
 def output(_video_path, cap):
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
+    fps = int(cap.get(5))
 
     outdir, inputflnm = _video_path[:_video_path.rfind(
-        '/')+1], _video_path[_video_path.rfind('/')+1:]
+        '/') + 1], _video_path[_video_path.rfind('/') + 1:]
 
     inflnm, inflext = inputflnm.split('.')
 
@@ -43,9 +45,9 @@ def output(_video_path, cap):
     except OSError:
         print("Error: Can't create a folder here.")
 
-    out_filename = f'{outdir}generateData_OUTPUT/{inflnm}_annotated.{inflext}'
-    out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc(
-        'M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+    out_filename = f'generateData_OUTPUT/{inflnm}_annotated.mp4'
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    out = cv2.VideoWriter(out_filename, fourcc, fps, (frame_width, frame_height))
     return out
 
 
@@ -54,16 +56,15 @@ def program(cap, out, mp_face_mesh, mp_drawing, mp_drawing_styles):
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
     face_mesh = mp_face_mesh.FaceMesh(
-            max_num_faces=1,
-            refine_landmarks=True,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5)
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5)
 
     count = 0
     while cap.isOpened():
         success, image = cap.read()
         if not success:
-            print(f"fail_{count}")
             count += 1
             break
 
@@ -73,7 +74,6 @@ def program(cap, out, mp_face_mesh, mp_drawing, mp_drawing_styles):
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
         '''OUTPUT MARKER'''
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
@@ -83,23 +83,26 @@ def program(cap, out, mp_face_mesh, mp_drawing, mp_drawing_styles):
                     connections=mp_face_mesh.FACEMESH_TESSELATION,
                     landmark_drawing_spec=None,
                     connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_tesselation_style())
+                        .get_default_face_mesh_tesselation_style())
                 mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
                     connections=mp_face_mesh.FACEMESH_FACE_OVAL,
                     landmark_drawing_spec=None,
                     connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_contours_style())
+                        .get_default_face_mesh_contours_style())
                 mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
                     connections=mp_face_mesh.FACEMESH_IRISES,
                     landmark_drawing_spec=None,
                     connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_iris_connections_style())
+                        .get_default_face_mesh_iris_connections_style())
+        # cv2.imshow('frame', image)
+        # if cv2.waitKey(5) & 0xFF == 27:
+        #     break
         out.write(image)
-
+    face_mesh.close()
     cap.release()
     out.release()
 
